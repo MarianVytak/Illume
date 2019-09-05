f9aa9ea6a89fdd26183fc8dcb20c7b167d9f7b5b
MarianVytak <marianvytak@gmail.com>
2019-09-05 22:17:07 +0300

    Added Custom Scrollbar

---
+++ src/blocks/modules/home-3/home-3.js
@@ -0,0 +1,8 @@
+import $ from 'jquery';
+import '../../../../node_modules/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min';
+
+// Custom Scrollbar
+$('.home-3__slider').mCustomScrollbar({
+    theme:"minimal-dark",
+    horizontalScroll: true
+});
--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,3 +1,4 @@
 import "%modules%/header/header";
 import "%modules%/home-2/home-2";
+import "%modules%/home-3/home-3";
 import "%modules%/footer/footer";
