e10c3811c454da3ba6c22fa5a6fbfccd6e432346
MarianVytak <marianvytak@gmail.com>
2019-09-14 23:27:31 +0300

    Added Footer

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -4,6 +4,5 @@ import "%modules%/home-3/home-3";
 import "%modules%/home-9/home-9";
 import "%modules%/home-11/home-11";
 import "%modules%/home-13/home-13";
-import "%modules%/footer/footer";
 import "%modules%/hamburger/hamburger";
 import "./libs/parallax";
